82ca8d59a2b9b7d5ce206694b1ce4cb87f76366a
Danny Chang <dannychachang@gmail.com>
2018-01-15 17:22:32 -0500

    update ballot to work

--- intro_to_web_applications/First_web_app/server.rb
+++ intro_to_web_applications/First_web_app/server.rb
@@ -3,3 +3,27 @@ require 'sinatra'
 get '/home' do
   erb:index
 end
+
+get '/' do
+  redirect to('/home')
+end
+
+
+get '/portfolio' do
+  erb:gallery
+end
+
+get '/gallery' do
+  redirect to('/portfolio')
+end
+
+get '/about_me' do
+  @skills = ['headspins', 'perfect pitch', 'I can run faster than a car', 'See Es Es', 'Backflip over a stack of 5 chairs']
+  @interests = ['chilling in general', 'kitty martial arts', 'art for winners', 'music for babies', 'films for the courageous', 'coffee brewed from tibetan monks']
+  erb:about_me
+end
+
+get '/favorites' do
+  @fave_links = ["google.com", "hover.com", "amazon.ca", "reddit.com"]
+  erb:favorites
+end
